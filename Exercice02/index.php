b3a92697ed2cdea94838a0009e6fa80c54815d16
Biguede <biguedeboudeliou@gmail.com>
2020-12-09 14:07:03 +0100

    Fin de l'exercice  2

---
+++ Exercice02/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+.blue {
+    background-color: blue;
+}
+</style>
+<body>
+<?php
+
+$name = "Biguede";
+$color = "blue";
+echo '<h1> Bienvenue à toi ' .$name. '.</h1>';
+echo '<body class='.$color.'></body>';
+
+
+?>
+    <h1></h1>
+</body>
+</html>
