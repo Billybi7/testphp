9d708ee8617f38bb035a4be0f5956064b34d8eb7
Biguede <biguedeboudeliou@gmail.com>
2020-12-09 16:16:51 +0100

    Fin de l'exercice 03

--- Exercice03/index.php
+++ Exercice03/index.php
@@ -1,20 +1,34 @@
+<?php
+
+
+$admin = false;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    .error {
+        font-weight:bold;
+        color:red;
+
+    }
+    </style>
 </head>
 <body>
+    <h1> Exercice 3</h1>
+
+
     <?php
-
-
-$admin = false;
-
-    if($admin == TRUE){
-        echo '<p> Bienvenue dans votre domaine monsieur</p> <br> <a href=#>Lien externe</a>';
+    if($admin){
+        ?>
+        <p> Bienvenue dans votre domaine monsieur</p> <br> <a href=#>Lien externe</a>
+        <?php
     } else {
-        echo '<p style= background-color:red;> Page réservée aux admins </p>';
+        ?> <p class="error"> Page réservée aux admins </p> 
+        <?php
     }
     ?>
 </body>
