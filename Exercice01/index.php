6c3b1d81b33ab1df8a70afabba2b0aee036ae689
Biguede <biguedeboudeliou@gmail.com>
2020-12-09 11:46:34 +0100

    Fin de l'exercice 1

---
+++ Exercice01/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exercice01</h1>
+
+    <?php 
+
+    echo '<ul> 
+            <li>Orange</li>
+            <li>Pomme</li>
+            <li>Fraise</li>
+            <li>Kiwi</li>
+            <li>Banane</li>
+            </ul>';
+
+    ?>
+</body>
+</html>
