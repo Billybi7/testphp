30cf66a9077ca83cd42f1e559c7dc87f44cf859f
Biguede <biguedeboudeliou@gmail.com>
2020-12-10 15:01:22 +0100

    Bonus exercice 08

--- Exercice08/index.php
+++ Exercice08/index.php
@@ -7,30 +7,43 @@
             'age' => '24',
             'country' => 'france',
             'city' => 'mulhouse',
+            'countrys' => [
+                'algérie', 'angleterre'
+            ],
         ],
         [
             'name' => 'bobi',
             'age' => '20',
             'country' => 'france',
             'city' => 'strasbourg',
+            'countrys' => [
+                'allemagne', 'maroc'
+            ],
         ],
         [
             'name' => 'bibi',
             'age' => '16',
             'country' => 'france',
             'city' => 'rixheim',
+            'countrys' => [
+                'espagne', 'italie'
+            ],
         ],
         [
             'name' => 'bilibi',
             'age' => '13',
             'country' => 'france',
             'city' => 'kingersheim',
+            'countrys' => [
+                'suisse', 'canada'
+            ],
         ],
         [
             'name' => 'bilou',
             'age' => '4',
             'country' => 'france',
             'city' => 'lutterbach',
+            'countrys' => [''],
         ],
     ];            
 
@@ -48,11 +61,22 @@
     <?php
     foreach( $users as $user){
        
-        echo '<h2> ' .$user['name']. '</h2> <p> Âgé de ' .$user['age']. ' vivant en ' .$user['country']. ' à ' .$user['city']. '. </p>';
+        echo '<h2> ' .$user['name']. '</h2> <p> Âgé de ' .$user['age']. ' vivant en ' .$user['country']. ' à ' .$user['city']. '. </p>';{
+            if(count($user["countrys"]) > 1) {
+
+                ?>
+                <ul>
+                <?php
+                echo '<br> Voici les pays visités ' .$user['countrys'][0]. ' et ' .$user['countrys'][1]. ".";
+                ?>
+                </ul>
+                <?php
+            } else {
+                echo $user['name']." n'a pas encore visité de pays.";
+            }
         
     }
-
-
+}
 
     ?>
 </body>
