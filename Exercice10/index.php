0477c20aa0f610af78b29529f0fb8cf5a3af9533
Biguede <biguedeboudeliou@gmail.com>
2020-12-11 15:28:05 +0100

    fin de l'éxercice 10 b

--- Exercice10/index.php
+++ Exercice10/index.php
@@ -2,4 +2,11 @@
 
 // Exercice 10-a : Afficher la date avec la fonction date sous le format suivant : "friday 11 december 2020, 14h 55m 30s"
 
-echo date('l j F Y H\h i\m s\s');
+echo date('l j F Y, H\h i\m s\s ');
+?><br> <hr>
+<?php
+// Exercice 10-b : Chercher à afficher la date avec strftime en français en vous aidant de google et php.net
+
+setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']);
+echo strftime("%A %d %B %Y, %Hh %Mm %Ss");
+
