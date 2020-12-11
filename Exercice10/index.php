d62ed56a454cbdcdb3209ba3ac52a2fe90537bea
Biguede <biguedeboudeliou@gmail.com>
2020-12-11 15:04:42 +0100

    Exercice 10 a

--- Exercice10/index.php
+++ Exercice10/index.php
@@ -2,4 +2,4 @@
 
 // Exercice 10-a : Afficher la date avec la fonction date sous le format suivant : "friday 11 december 2020, 14h 55m 30s"
 
-echo date('d-m-Y H\h i\m s\s');
+echo date('l j F Y H\h i\m s\s');
