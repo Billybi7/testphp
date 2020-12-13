117a49622e2675de7ea4d7492c6e4e783de01a10
Biguede <biguedeboudeliou@gmail.com>
2020-12-13 17:47:46 +0100

    Fin de l'exercice 10d weekend

--- Exercice10/index.php
+++ Exercice10/index.php
@@ -10,4 +10,25 @@ echo date('l j F Y, H\h i\m s\s ');
 setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']);
 echo strftime("%A %d %B %Y, %Hh %Mm %Ss <hr>");
 
-echo utf8_encode(strftime("%A %d %B %Y, %Hh %Mm %Ss"));
+echo utf8_encode(strftime("%A %d %B %Y, %Hh %Mm %Ss <hr>"));
+
+$currentDate = date('d-m-Y H:i:s');
+?>
+
+<?php
+echo $currentDate. '<hr>';
+
+
+// Exercice 10-c :Avec la fonction date(), afficher à l'écran la date qu'il sera dans 26 jours et 16h 2020-12-11 06:42:30
+
+
+echo date("Y-m-d, H:i:s ", time()+(26*24*3600)+(16*3600));
+echo '<br><hr>';
+
+
+// Exercice 10-d : Créer une variable contenant cette date précise textuelle : "2004-04-16 12:00:00". Le but est d'ajouter 435 jours à cette date puis de l'afficher sous le format suivant : "samedi 25 juin 2005, 06h 00m 00s"
+
+$dateToTransform ="2004-04-16 12:00:00";
+echo '  Exo d<br><hr>';
+setlocale(LC_TIME, "fr_FR");
+echo strftime("%A %d %B %Y, %Hh %Mm %Ss <hr>", strtotime($dateToTransform. ' + 435 days'));
