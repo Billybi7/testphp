c825928115688c12c02109698323ab81eda4aa27
Biguede <biguedeboudeliou@gmail.com>
2020-12-14 11:05:36 +0100

    Exo 11

---
+++ Exercice11/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Accueil</h1>
+    <nav>
+        <ul>
+        <ul><a href="/index.php">Accueil</a></ul>
+        <ul><a href="/contact.php">Contact</a></ul>
+        <ul><a href="/shop.php">Boutique</a></ul>
+        </ul>
+    </nav>
+</body>
+</html>
